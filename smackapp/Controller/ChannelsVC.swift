fa85406996bd67354af6542ccafcc03e0147bc3f
Tapiwa Jiri <tapiwajiri@Tapiwas-MacBook-Pro.local>
2017-09-20 15:10:33 +0200

    working so far no code only VCs done

---
+++ smackapp/Controller/ChannelsVC.swift
@@ -0,0 +1,35 @@
+//
+//  ChannelsVC.swift
+//  smackapp
+//
+//  Created by Tapiwa Jiri on 2017/09/20.
+//  Copyright © 2017 Tapiwa Jiri. All rights reserved.
+//
+
+import UIKit
+
+class ChannelsVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ smackapp/Controller/ChatVC.swift
@@ -0,0 +1,22 @@
+//
+//  ChatVC.swift
+//  smackapp
+//
+//  Created by Tapiwa Jiri on 2017/09/20.
+//  Copyright © 2017 Tapiwa Jiri. All rights reserved.
+//
+
+import UIKit
+
+class ChatVC: UIViewController {
+    @IBOutlet weak var menuBtn: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+       
+    }
+
+    
+
+}
