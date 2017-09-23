a39ca4c485e780545137db87ed55038342db0fc4
Tapiwa Jiri <tapiwajiri@Tapiwas-MacBook-Pro.local>
2017-09-23 15:21:39 +0200

    lesson 4

---
+++ smackapp/Controller/loginVC.swift
@@ -0,0 +1,22 @@
+//
+//  loginVC.swift
+//  smackapp
+//
+//  Created by Tapiwa Jiri on 2017/09/23.
+//  Copyright © 2017 Tapiwa Jiri. All rights reserved.
+//
+
+import UIKit
+
+class loginVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    @IBAction func closePressed(_ sender: Any) { dismiss(animated: true, completion: nil)}
+    
+    
+  
+
+}
---
+++ smackapp/utilities/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  smackapp
+//
+//  Created by Tapiwa Jiri on 2017/09/23.
+//  Copyright © 2017 Tapiwa Jiri. All rights reserved.
+//
+
+import Foundation
+
+// segues
+
+let TO_LOGIN = "toLogin"
